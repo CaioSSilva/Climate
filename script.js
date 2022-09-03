2c6a69cc58298f439fcc35de0ccec5b8d133ef13
Caio Souza Silva <caiosouzasilva13650@gmail.com>
2022-09-03 17:27:38 -0300

    error add

--- script.js
+++ script.js
@@ -10,38 +10,46 @@ function getData(){
     let inputValue = placeInput.value
     const BASE_URL = `https://api.openweathermap.org/data/2.5/weather?q=${inputValue}&appid=${API_KEY}&units=metric&lang=pt_br`
 
-    try{
-        fetch(BASE_URL)
-        .then((promise) => promise.json())
-        .then(data => {
-            let clima = data.weather.map(clima => clima.description)
-            let iconNumber = data.weather.map(clima => clima.icon)
-            let temp = data.main.temp
-            let temp_min = data.main.temp_min
-            let temp_max = data.main.temp_max
-            let feelslike = data.main.feels_like
-            let humidity = data.main.humidity
-            let pressure = data.main.pressure
-            let iconPath = `https://openweathermap.org/img/wn/${iconNumber}@2x.png`
-            starter.style.height = '50vh'
-            results.style.display = "flex"
-            results.innerHTML = `
-            <div>
-                <img src=${iconPath} />
-            </div>
-            <div>
-                <h1>${data.name}</h1>
-                <p>Clima: ${clima}</p>
-                <p>Sensação Termica: ${feelslike} ºC</p>
-                <p>Temperatura: ${temp} ºC</p>
-                <p>Temperatura-Maxima: ${temp_max} ºC</p>
-                <p>Temperatura-Minima: ${temp_min} ºC</p>
-                <p>Umidade: ${humidity} %</p>
-                <p>Pressão: ${pressure} hPa</p>
-            </div>`
-        })
-    }
-    catch (e){
-        console.log(e.data)
-    }
+    fetch(BASE_URL)
+    .then((promise) => promise.json())
+    .then(data => {
+        let clima = data.weather.map(clima => clima.description)
+        let iconNumber = data.weather.map(clima => clima.icon)
+        let temp = data.main.temp
+        let temp_min = data.main.temp_min
+        let temp_max = data.main.temp_max
+        let feelslike = data.main.feels_like
+        let humidity = data.main.humidity
+        let pressure = data.main.pressure
+        let iconPath = `https://openweathermap.org/img/wn/${iconNumber}@2x.png`
+        starter.style.height = '50vh'
+        results.style.display = "flex"
+        results.innerHTML = `
+        <div>
+            <img src=${iconPath} />
+        </div>
+        <div>
+            <h1>${data.name}</h1>
+            <p>Clima: ${clima}</p>
+            <p>Sensação Termica: ${feelslike} ºC</p>
+            <p>Temperatura: ${temp} ºC</p>
+            <p>Temperatura-Maxima: ${temp_max} ºC</p>
+            <p>Temperatura-Minima: ${temp_min} ºC</p>
+            <p>Umidade: ${humidity} %</p>
+            <p>Pressão: ${pressure} hPa</p>
+        </div>`
+    })
+    .catch((e) => {
+        starter.style.height = '50vh'
+        results.style.display = "flex"
+        results.innerHTML = `
+        <div>
+            <img id="error-img" src="https://cdn-icons-png.flaticon.com/512/753/753345.png" width:50px/>
+        </div>
+        <div>
+            <h1>Localização não encontrada!</h1>
+            <p>Tente novamente...</p>
+        </div>`
+
+    })
 }
